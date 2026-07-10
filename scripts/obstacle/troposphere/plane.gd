bec070ff3743be0cd1f49a8fca9d4145dbbcd651
Alowna <alonum2004@gmail.com>
2026-07-10 16:35:53 -0300

    refactor: movement responsabilities transfered to Obstacle class

--- scripts/obstacle/troposphere/plane.gd
+++ scripts/obstacle/troposphere/plane.gd
@@ -1,11 +1,5 @@
 extends Obstacle
 
-
-#Plane physics
-func _physics_process(delta):
-	var direction = Vector2.RIGHT
-	# Movement direction of the plane.
-	var speed := 70.0
-	# Movement speed of the plane.
-	
-	move_obstacle(direction, speed)
+func _ready():
+	z_index = 2
+	speed = 100.0
