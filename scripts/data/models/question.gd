00b67ad04201763a6f77d219c95f3ee9891de176
Alowna <alonum2004@gmail.com>
2026-07-11 16:23:09 -0300

    feat: created question and question provider models for local testing

---
+++ scripts/data/models/question.gd
@@ -0,0 +1,9 @@
+extends RefCounted
+
+class_name Question
+
+var id: int
+var category_id: int
+var text: String
+var answers: Array[String]
+var correct_answer: int
---
+++ scripts/data/providers/question_provider.gd
@@ -0,0 +1,5 @@
+class_name QuestionProvider
+extends Question
+
+func get_questions(category_id: int) -> Array[Question]:
+	return []
